aaa730459c725dc1cf18f1ee5893e4dbf9a256f4
Nishanthdon <nishanthsubburaj100@gmail.com>
2017-05-19 17:47:58 +0530

    Add files via upload

--- Shoppingcart Front End/src/main/java/com/niit/controller/CategoryController.java
+++ Shoppingcart Front End/src/main/java/com/niit/controller/CategoryController.java
@@ -20,10 +20,10 @@ public class CategoryController {
 
 	@RequestMapping("newCategory")
 	public String submit(@ModelAttribute Category category, Model model) {
-		boolean flag = categoryDAO.save(category);
+		boolean flag = categoryDAO.saveOrUpdate(category);
 		model.addAttribute("isUserClickedView_Category", "true");
 		System.out.println(flag);
-		return "adminLogin";
+		return "redirect:/viewcategoryPage";
 
 	}
 
@@ -58,6 +58,27 @@ public class CategoryController {
 
 		return "adminLogin";
 	}
+	
+
+@RequestMapping("editCategory")
+	public String editCategory(@RequestParam(value = "categoryId") String id, Model model) {
+
+		Category category = categoryDAO.get(id);
+		model.addAttribute("category", category);
+		model.addAttribute("isUserClickedEdit", "true");
+		return "adminLogin";
+
+	}
+
+@RequestMapping("afterEditCategory")
+	public String afterEditCategory(@ModelAttribute Category category, Model model){
+		categoryDAO.saveOrUpdate(category);
+		
+		return "redirect:/viewcategoryPage";
+		
+	}
+
+	
 
 	@ModelAttribute
 	public void adminCategory(Model model) {
